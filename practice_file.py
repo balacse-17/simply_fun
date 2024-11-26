73094db4162c50314d5489288c34faf9c3070ed7
balasubramaniyanS <balasm2007@gmail.com>
2024-11-26 19:04:14 +0530

    bala

--- practice_file.py
+++ practice_file.py
@@ -19,9 +19,12 @@ def exp(a,b):
 def root(a):
     return a**0.5
 
-st.write("CALCULATOR")
-a=st.number_input("number-1")
-b=st.number_input("number-2")
+def cubic_root(a):
+    return a**(1/3)
+
+st.header("CALCULATOR")
+a=st.number_input("Number-1")
+b=st.number_input("Number-2")
 
 
 col1,col2,col3,col4,col5 = st.columns(5)
@@ -44,13 +47,13 @@ with col3:
     st.button("=")
 
 with col4:
-    if st.button("add"):
+    if st.button("Add"):
         ans=add(a,b)
-    if st.button("sub"):
+    if st.button("Sub"):
         ans=sub(a,b)
-    if st.button("mul"):
+    if st.button("Mul"):
         ans=mul(a,b)
-    if st.button("div"):
+    if st.button("Div"):
         ans=div(a,b)
 
 with col5:
@@ -58,5 +61,7 @@ with col5:
         ans=exp(a,b)
     if st.button("Square Root"):
         ans=root(a)
+    if st.button("Cubic Root"):
+        ans=cubic_root(a)
 
 st.title(ans)
